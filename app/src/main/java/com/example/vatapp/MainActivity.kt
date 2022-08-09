57bd28287cf0a5c8866cb1de8a3914ff65d79204
Ghadeer-Alzughaibi <ghadeer.alzughaibi0@gmail.com>
2022-08-09 03:50:35 +0300

    add function calculateVat and final total.

--- app/src/main/java/com/example/vatapp/MainActivity.kt
+++ app/src/main/java/com/example/vatapp/MainActivity.kt
@@ -7,6 +7,7 @@ import android.widget.EditText
 import android.widget.RadioButton
 import android.widget.TextView
 import com.example.vatapp.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
 
@@ -29,7 +30,29 @@ class MainActivity : AppCompatActivity() {
     }// End onCreate
 
     private fun calculateVat() {
+        val stringInTextField = binding.etTotalCost.text.toString()
+        val cost = stringInTextField.toDouble()
 
+        val selected = binding.rgVatOption.checkedRadioButtonId
+        val vat = when (selected){
+            R.id.rb_vat_10 -> 0.10
+            R.id.rb_vat_15 -> 0.15
+            else -> 0.20
+        }
+
+        var vatCalc =  vat * cost
+        var total =  cost + vatCalc
+
+        val roundVat = binding.switchRoundUp.isChecked
+        if (roundVat){
+            total = kotlin.math.ceil(total)
+        }
+
+        val formattedVat = NumberFormat.getCurrencyInstance().format(vatCalc)
+        binding.txtVatTotal.text = getString(R.string.vat, formattedVat)
+
+        val formattedTotal = NumberFormat.getCurrencyInstance().format(total)
+        binding.txtTotal.text = getString(R.string.total_amount,formattedTotal)
     }
 
 }// End MainActivity
